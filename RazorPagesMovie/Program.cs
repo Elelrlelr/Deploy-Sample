a91737df590186e9cb4d6f6086d01d095ddd5316
unknown <admirallqh@gmail.com>
2022-06-25 18:22:35 +0700

    bit of tire

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -58,7 +58,7 @@ using (var scope = app.Services.CreateScope())
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
